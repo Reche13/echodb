8a057f4d5e1078f0a03c533e2080e8395b09f8ca
Reche Soares <rechesoares13@gmail.com>
2025-10-13 08:49:47 +0530

    initialized server and parser

--- main.go
+++ main.go
@@ -2,8 +2,91 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
 )
 
+type ValueType string
+
+const (
+	ARRAY ValueType = "*"
+	BULK ValueType = "$"
+	STRING ValueType = "+"
+)
+
+type Value struct {
+	typ ValueType
+	bulk string
+	str string
+	array []Value
+}
+
+func (v *Value) readArray(reader io.Reader) {
+	buf := make([]byte, 4)
+	_, err := reader.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	arrLen, err := strconv.Atoi(string(buf[1]))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for range arrLen {
+		bulk := v.readBulk(reader)
+		v.array = append(v.array, bulk)
+	}
+}
+
+func (v *Value) readBulk(reader io.Reader) Value {
+	buf := make([]byte, 2)
+	_, err := reader.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return Value{}
+	}
+
+	n, err := strconv.Atoi(string(buf[1]))
+	if err != nil {
+		fmt.Println(err)
+		return Value{}
+	}
+
+	bulkBuff := make([]byte, n+2)
+	_, err = reader.Read(bulkBuff)
+	if err != nil {
+		fmt.Println(err)
+		return Value{}
+	}
+
+	bulk := string(bulkBuff[:n])
+	return Value{typ: BULK, bulk: bulk}
+}
+
 func main() {
-	fmt.Println("ECHO DB")
+	l, err := net.Listen("tcp", ":6380")
+	if err != nil {
+		log.Fatal("cannot listen on port :6380")
+	}
+	defer l.Close()
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	for {
+		v := Value{typ: ARRAY}
+		v.readArray(conn)
+		fmt.Println(v.array)
+
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
