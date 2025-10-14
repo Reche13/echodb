80657dda7fa99dc1c48dffcd166a52bc293df421
Reche Soares <rechesoares13@gmail.com>
2025-10-14 11:13:37 +0530

    add echo command

---
+++ internal/commands/echo.go
@@ -0,0 +1,20 @@
+package commands
+
+import "github.com/reche13/echodb/internal/protocol"
+
+func init() {
+	Register("ECHO", Echo)
+}
+
+func Echo(args []*protocol.RESPValue) *protocol.RESPValue {
+	if len(args) != 1 {
+		return protocol.NewError("ERR wrong number of arguments for 'ECHO' command")
+	}
+	
+	str, ok := args[0].GetString()
+	if !ok {
+		return protocol.NewError("ERR invalid argument for 'ECHO' command")
+	}
+
+	return protocol.NewSimpleString(str)
+}
